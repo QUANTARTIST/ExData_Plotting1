d0f6ff9b843c4e592e24ffc7d4d0e738515fde74
QUANTARTIST <Lucy.Nicholls@nielsen.com>
2016-07-02 12:13:37 -0500

    commit

---
+++ plot1.R
@@ -0,0 +1,10 @@
+##To extract the entire data set
+data <- read.csv("C:/ExData_Plotting1/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+## To subset the data
+data_set <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data)
+## Converting dates
+datetime <- paste(as.Date(data_set$Date), data_set$Time)
+data_set$Datetime <- as.POSIXct(datetime)
